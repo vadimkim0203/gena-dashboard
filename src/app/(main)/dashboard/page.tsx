2daf706b4ed3640add2b430e1bd4076146cbb29a
Vadim Kim <vkim0203@gmail.com>
2025-07-10 16:34:30 +0900

    initial commit

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+const Dashboard = () => {
+    return (
+        <div className="flex flex-col items-center justify-center border-2 border-red-500">Hello World!</div>
+    )
+}
+
+export default Dashboard
